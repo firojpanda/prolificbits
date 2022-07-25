f44e857e56d0bf096f303e16dd1d4a727b091cc7
firojpanda <firoj.panda@outlook.com>
2022-07-25 12:19:49 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 provider "google" {
-project = "qwiklabs-gcp-04-576cc70c1ddb"
-region = "us-west1"
+project = "649885794631"
+region = "us-central"
 }
 
-resource "google_compute_instance" "myvm" {
-name = "myvm-dev"
-zone = "us-west1-c"
+resource "google_compute_instance" "pocvm" {
+name = "vm_tf_gcp_git"
+zone = "us-central1-a"
 boot_disk {
 initialize_params {
   image = "debian-cloud/debian-9"
