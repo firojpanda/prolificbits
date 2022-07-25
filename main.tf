1f2864ae48c6cda41be59cbd6a4e04201cc8fefa
firojpanda <firoj.panda@outlook.com>
2022-07-25 16:09:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,18 @@ network_interface {
 network = "default"
 }
 }
+
+resource "google_compute_instance" "vm_instance2" {
+name = "vm-tf-gcp-git-2"
+zone = "us-central1-a"
+boot_disk {
+initialize_params {
+  image = "debian-cloud/debian-9"
+  size = "10"
+}
+}
+machine_type = "f1-micro" 
+network_interface {
+network = "default"
+}
+}
