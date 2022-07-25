fd38ce600790f564ea5973704743a9eb1093b571
firojpanda <firoj.panda@outlook.com>
2022-07-25 11:32:22 -0400

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,19 @@
+rovider "google" {
+project = "qwiklabs-gcp-04-576cc70c1ddb"
+region = "us-west1"
+}
+
+resource "google_compute_instance" "myvm" {
+name = "myvm-dev"
+zone = "us-west1-c"
+boot_disk {
+initialize_params {
+  image = "debian-cloud/debian-9"
+  size = "10"
+}
+}
+machine_type = "f1-micro" 
+network_interface {
+network = "default"
+}
+}
