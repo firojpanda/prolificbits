9fd68d27ad0863da8a9826c73ae0b2e85108dd95
firojpanda <firoj.panda@outlook.com>
2022-07-25 14:17:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,21 +3,6 @@ project = "649885794631"
 region = "us-central"
 }
 
-resource "google_compute_instance" "vm_instance" {
-name = "vm-tf-gcp-git-1"
-zone = "us-central1-a"
-boot_disk {
-initialize_params {
-  image = "debian-cloud/debian-9"
-  size = "10"
-}
-}
-machine_type = "f1-micro" 
-network_interface {
-network = "default"
-}
-}
-
 resource "google_compute_instance" "vm_instance2" {
 name = "vm-tf-gcp-git-2"
 zone = "us-central1-a"
